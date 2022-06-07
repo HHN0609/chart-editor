b99e4d18d443b948d52a6c4e6aef98663405016b
hehaonan <hehaonan@corp.netease.com>
2022-06-07 18:07:21 +0800

    初步引入moveablejs

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -2,11 +2,34 @@
   <div class="mainBox">
     <vue-infinite-viewer :draggable="true" :zoom="10" class="viewer">
       <div>AA</div>
+      <div class="target" ref="target">Vue Moveable</div>
+      <Moveable
+          class="moveable"
+          v-bind:target="['.target']"
+          v-bind:draggable="true"
+          v-bind:scalable="true"
+          v-bind:rotatable="true"
+          @drag="onDrag"
+          @scale="onScale"
+          @rotate="onRotate"
+      />
     </vue-infinite-viewer>
   </div>
 </template>
 <script lang="ts" setup>
+import { ref } from "vue";
 import { VueInfiniteViewer } from "vue3-infinite-viewer";
+import Moveable from "vue3-moveable";
+const target = ref(null);
+const onDrag = ({ transform }) => {
+  target.style.transform = transform;
+}
+const onScale = ({ drag }) => {
+  target.style.transform = drag.transform;
+}
+const onRotate = ({ drag }) => {
+  target.style.transform = drag.transform;
+}
 </script>
 <style lang="less" scoped>
   .mainBox {
