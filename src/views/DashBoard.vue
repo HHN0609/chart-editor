0cee0ac8520cea7f788448a4a1c03567f09eaea6
hehaonan <hehaonan@corp.netease.com>
2022-06-08 18:08:12 +0800

    1.完成DashBoard画布的滚动和缩放

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,44 +1,66 @@
 <template>
-  <div class="mainBox">
-    <vue-infinite-viewer
-      :draggable="true"
-      :zoom="1"
-      :rangeX="[-5000, 5000]"
-      :rangeY="[-5000, 5000]"
-      @drag="onDrag"
-      class="viewer">
-      <div class="viewport">AA</div>
-    </vue-infinite-viewer>
+  <div class="mainbox">
+  <vue-infinite-viewer
+    class="viewer"
+    ref="viewer"
+    v-bind="viewerOptions"
+    @scroll.once="getInstance"
+    >
+    <div class="viewport"></div>
+  </vue-infinite-viewer>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+import { onUnmounted, reactive, ref } from "vue";
+import InfiniteViewer , { InfiniteViewerOptions, OnPinch, OnDrag, OnScroll } from "infinite-viewer";
 import { VueInfiniteViewer } from "vue3-infinite-viewer";
-import Moveable from "vue3-moveable";
-// const target = ref(null);
-const onDrag = (e) => {
-  console.log(e.inputEvent);
+import Guides from "@scena/guides";
+let guideHorizontal: Guides;
+let guideVertical: Guides;
+let infiniteViewer: InfiniteViewer;
+let viewerOptions = reactive<Partial<InfiniteViewerOptions>>({
+  zoom: 1,
+  rangeX: [-1000, 1000],
+  rangeY: [-1000, 1000],
+  usePinch: true,
+  maxPinchWheel: 10,
+});
+
+const getInstance = (e) => {
+  infiniteViewer = e.currentTarget;
+  infiniteViewer.scrollCenter();
+  infiniteViewer.on("pinch", (e: OnPinch) => {
+    console.log(e);
+    infiniteViewer.setZoom(e.zoom);
+  });
+
+  infiniteViewer.on("drag", (e: OnDrag) => {
+    console.log(e);
+  });
 }
-// const onScale = ({ drag }) => {
-//   target.style.transform = drag.transform;
-// }
-// const onRotate = ({ drag }) => {
-//   target.style.transform = drag.transform;
-// }
+onUnmounted(() => {
+  infiniteViewer.off();
+  infiniteViewer.destroy();
+  // 这里要记录一些图表的数据
+})
+
 </script>
 <style lang="less" scoped>
-  .mainBox {
+  .mainbox {
     width: 100%;
     min-height: 100%;
     > .viewer {
+      overflow: hidden;
       width: 80%;
       height: 90vh;
+      border: black 1px solid;
       overflow: hidden;
       background:gainsboro;
       .viewport{
-        width: 500px;
-        height: 500px;
-        background-color:aqua;
+        overflow: hidden;
+        width: 800px;
+        height: 450px;
+        background-color:rgb(39, 44, 44);
       }
     }
   }
